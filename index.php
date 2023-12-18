5eea0f91e18bfc9d1f3fb13c5909d222988a6cba
amir talaat <hsiwh2be872b@gmail.com>
2023-12-18 14:04:07 +0200

    sex

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+
+class User {
+  public $name;
+  private $password;
+  private $email;
+  public const x = 15;
+
+  public function __construct($name, $password, $email) {
+    $this->name = $name;
+    $this->password = $password;
+    $this->email = $email;
+  }
+
+  public function get_name() {
+    return $this->name;
+  }
+
+  public function get_password() {
+    return $this->password;
+  }
+
+  public function get_email() {
+    return $this->email;
+  }
+
+  public function set_name($name) {
+    $this->email = $name;
+  }
+
+  public function set_password($password) {
+    $this->password = $password;
+  }
+
+  public function set_email($email) {
+    $this->email = $email;
+  }
+
+  public function get_self() {
+    return self::x;
+  }
+}
+
+$user1 = new User("amir", "26121982", "amir@gmail.com");
+
+// encapsulation
+echo ("-------- encapsulation --------" . "<br>");
+echo $user1->get_name() . "<br>";
+echo $user1->get_email() . "<br>";
+echo $user1->get_password() . "<br>";
+$user1->set_password("28102006");
+echo $user1->get_password() . "<br>";
+echo $user1->get_self() . "<br>";
+
